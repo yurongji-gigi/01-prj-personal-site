7f8f780519bb9c4a9d554cb3041eb5b58bed04e3
yurongji-gigi <yurongji@asu.edu>
2026-09-15 23:37:34 +0000

    change

---
+++ script.js
@@ -0,0 +1,18 @@
+document.documentElement.classList.add("js-enabled");
+
+const revealItems = document.querySelectorAll("[data-reveal]");
+
+if ("IntersectionObserver" in window) {
+  const revealObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("is-visible");
+        observer.unobserve(entry.target);
+      }
+    });
+  }, { threshold: 0.12 });
+
+  revealItems.forEach((item) => revealObserver.observe(item));
+} else {
+  revealItems.forEach((item) => item.classList.add("is-visible"));
+}
